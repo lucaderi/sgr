6225f5060113bc21cb53a7b4d1686aad7eed9500
Simone Russo <russo96.simone@gmail.com>
2018-06-28 17:15:28 +0200

    Update table_updater.js

--- 2018/Armato_Russo/js/table_updater.js
+++ 2018/Armato_Russo/js/table_updater.js
@@ -7,11 +7,11 @@ function update_social_table(ip) {
 			var metrics=JSON.parse(content)
         	var col = [];
         	for (var i = 0; i < metrics.length; i++) {
-            	for (var key in metrics[i]) {
-                	if (col.indexOf(key) === -1) {
-                    		col.push(key);
-                	}
-            	}
+            		for (var key in metrics[i]) {
+                		if (col.indexOf(key) === -1) {
+                    			col.push(key);
+                		}
+            		}
         	}
         	var table = document.createElement("table");
         	var tr = table.insertRow(-1);

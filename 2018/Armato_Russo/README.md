69aed0459203782bcb878ab8c70892d5aa0432f5
simo-r <russo96.simone@gmail.com>
2018-06-28 14:11:40 +0200

    Progetto Gestione di Rete

---
+++ 2018/Armato_Russo/README.md
@@ -0,0 +1,9 @@
+# Progetto Gestione di Reti
+Il progetto ha lo scopo di utilizzare l' API di ntopng per analizzare il traffico riguardante i social network all'interno della rete. Gli script messi a disposizione mostrano statistiche che riguardano pacchetti, bytes e durata dei flussi social di un determinato host.
+# Requisiti
+-ntopng
+# Uso
+- Copiare ~/[nomeprogetto]/lua/* in ~/ntopng/scripts/lua
+- Copiare i file ~/[nomeprogetto]/css/* in ~/ntopng/httpdocs/css
+- Copiare i file ~/[nomeprogetto]/js/* in ~/ntopng/httpdocs/js
+- Visitare la pagina web [host:port]/lua/host_social_details.lua
---
+++ 2018/Armato_Russo/lua/social_stats.lua
@@ -0,0 +1,107 @@
+-- [DONE] Parametrizzare per host ip
+-- [DONE] Parametrizzare per il tipo di grafico (durata,byte,packets)
+-- [DONE] Accesso costante ai flussi che appartengono a SocialNetwork
+-- [DONE] Restituire un json con tutte le metriche per tutti i proto social per poterne fare una tabella
+-- [TODO] Cosa deve fare nel caso in cui c'è qualcosa che non va
+
+
+--[[ FUNCTIONS --]] 
+
+-- Stampa il valore di tutte le metriche di un protocollo social
+function allMetrics (protoNdpiStats)
+	local firstTime = true
+	for key,value in pairs(protoNdpiStats) do
+		if(type(value) == "table") then
+			for key2,value2 in pairs(value) do
+				if( not firstTime) then
+				print(", ")
+				end
+				print("\""..(key:sub(1,1):upper()..key:sub(2)).." "..(key2:sub(1,1):upper()..key2:sub(2)).."\": ".. value2)
+				firstTime = false
+			end
+		else -- if table
+			if( not firstTime) then
+				print(", ")
+			end
+			print("\""..(key:sub(1,1):upper()..key:sub(2)).."\": ".. value)
+		end
+		firstTime = false
+	end -- for protoNdpiStats
+	print("}")
+end
+
+-- Stampa il valore della metrica di un protocollo social
+function singleMetric (protoNdpiStats, metric)
+	protoValue = protoNdpiStats[metric]
+	if( metric == "bytes" or metric == "packets") then
+		protoValue = protoValue["sent"] + protoValue["rcvd"]
+	end	
+	print("\"value\": ".. protoValue .." }")
+end
+
+--[[ END FUNCTIONS --]] 
+
+dirs = ntop.getDirs()
+package.path = dirs.installdir .. "/scripts/lua/modules/?.lua;" .. package.path
+require "lua_utils"
+json = require("dkjson")
+sendHTTPContentTypeHeader('text/html')
+
+local host_ip = _GET["host"]
+--[[
+	Se metric = nil allora diamo tutti i valori delle metriche disponibili
+	altrimenti diamo solo la metrica richiesta tra le tre disponibili
+--]]
+local metric = _GET["p_metric"]
+hostInfo = interface.getHostInfo(host_ip)
+if(hostInfo == nil or (metric ~= nil and metric ~= "duration" and metric ~= "bytes" and metric ~= "packets")) then 
+	-- [TESTED] Controllo sui parametri
+	print "[ ]"
+	return 
+end
+
+jsonString = hostInfo.json
+--print("<p>"..jsonString.."<p>")
+local info, pos, err = json.decode(jsonString, 1, nil)
+if( err ) then
+	-- [TESTED]
+	print("[ ]")
+	return
+end
+
+stats = info.ndpiStats
+socialFlowExists = stats["categories"]["SocialNetwork"]
+if (socialFlowExists == nil) then
+	-- [TESTED] Se non c'è alcun flusso di tipo social
+	print("[ ]")
+	return 
+end
+
+-- Per capire dove mettere la virgola tra due oggetti json
+firstTime = true
+catId = interface.getnDPICategoryId("SocialNetwork")
+-- Table con tutti i protocolli con categoria SocialNetwork
+socialProtos = interface.getnDPIProtocols(catId)
+print "[ "
+for protoName,_ in pairs(socialProtos) do
+	protoNdpiStats = stats[protoName]
+	if(protoNdpiStats ~=nil) then
+		-- Se esiste un flusso per questo tipo di social
+		if( not firstTime) then
+			-- Dal secondo oggetto in poi la virgola tra il precedente e il corrente
+			print(",")
+		end
+		
+		if( metric == nil) then
+			print(" {\"Application Name\": \"".. protoName .."\", ")
+			allMetrics(protoNdpiStats)
+		else
+			print(" {\"label\": \"".. protoName .."\", ")
+			singleMetric(protoNdpiStats,metric)
+		end
+		firstTime = false
+	end -- if protoNdpiStats
+end -- for socialProtos
+print " ]"
+
+

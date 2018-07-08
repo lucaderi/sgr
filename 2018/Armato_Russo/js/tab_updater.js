417b7de7c167d6a63587f15fb82d5cad66156f69
Overflow404 <gaspare.armato1@gmail.com>
2018-07-08 13:19:30 +0200

    Eliminato intervallo di aggiornamento non necessario.

--- 2018/Armato_Russo/js/tab_updater.js
+++ 2018/Armato_Russo/js/tab_updater.js
@@ -1,16 +1,16 @@
-
 // Controlla se ci sono informazioni riguardanti i flussi social
 // ed in caso positivo abilita il tab social in host_details?host=[host_ip]
 function show_social_tab(ip){
 	$.ajax({
 		type : 'GET',
 		url: "/lua/social_stats",
-		data: "host="+ip,
+		data: "host="+ip+"&p_metric=packets",
 		success: function(content){
 			var metrics=JSON.parse(content)
 			if(metrics != ""){
 				var social_tab = document.getElementById('social_tab');
     			social_tab.style.display = "inline";
+                clearInterval(refreshId);
 			}
   		}
 	})

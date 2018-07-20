d92512a297b208ef9d86bcb189872fad96937566
Mariucci0 <mcoco96@libero.it>
2018-07-20 16:37:41 +0200

    Refactoring

--- 2018/Coco_Finocchio/structures.h
+++ 2018/Coco_Finocchio/structures.h
@@ -2,16 +2,16 @@
 
 /* Ethernet frame messaggi ARP */ 
 typedef struct eth_pkt { 
-	  struct ether_header ethdr;	/* Ethernet Header		        */
-    u_int16_t htype;    		    /* Tipo Hardware              */ 
-    u_int16_t ptype;    		    /* Protocollo                 */ 
-    u_char hlen;        		    /* Lunghezza indirizzo MAC    */ 
-    u_char plen;        		    /* Lunghezza indirizzo IP     */ 
-    u_int16_t oper;     		    /* Codice operazione          */ 
-    u_char sha[6];      		    /* Indirizzo MAC mittente     */ 
-    u_char spa[4];      		    /* Indirizzo IP mittente      */ 
-    u_char tha[6];      		    /* Indirizzo MAC destinatario */ 
-    u_char tpa[4];      		    /* Indirizzo IP destinatario  */ 
+  struct ether_header ethdr;    /* Ethernet Header            */
+  u_int16_t htype;              /* Tipo Hardware              */ 
+  u_int16_t ptype;              /* Protocollo                 */ 
+  u_char hlen;                  /* Lunghezza indirizzo MAC    */ 
+  u_char plen;                  /* Lunghezza indirizzo IP     */ 
+  u_int16_t oper;               /* Codice operazione          */ 
+  u_char sha[6];                /* Indirizzo MAC mittente     */ 
+  u_char spa[4];                /* Indirizzo IP mittente      */ 
+  u_char tha[6];                /* Indirizzo MAC destinatario */ 
+  u_char tpa[4];                /* Indirizzo IP destinatario  */ 
 }eth_pkt_t;
 
 
@@ -20,11 +20,11 @@ typedef struct hash_node {
   u_char sha[6];    /* Indirizzo MAC mittente */ 
   u_char spa[4];    /* Indirizzo IP mittente  */
   int lastUsed;     /* Flag di presenza       */
-} hash_node_t;
+}hash_node_t;
 
 
 /* Struttura statistiche rete */
 typedef struct net_stats {
-  int len;		/* Numero di utenti globale */
-  int online;	/* Numero di utenti online 	*/
+  int len;      /* Numero di utenti globale */
+  int online;   /* Numero di utenti online  */
 }net_stats_t;

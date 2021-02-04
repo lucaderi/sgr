0955bb1e7c9efcebb4db78710b826ade9dc4a156
Matteo Fantozzi <65367998+irfanto05@users.noreply.github.com>
2021-02-04 10:01:54 +0100

    Update snmp_monitorRamProcesses.py

--- 2021/Fantozzi/snmp_monitorRamProcesses.py
+++ 2021/Fantozzi/snmp_monitorRamProcesses.py
@@ -1,3 +1,5 @@
+## Developed by Matteo Fantozzi
+
 from easysnmp import Session
 from easysnmp import snmp_get
 import sys
@@ -111,18 +113,13 @@ if (os.path.isdir("reports")) == False:
     os.mkdir("reports")
     
 ## Print some info
-try:
-    print("Start COLLECTING DATA at: ", hostname," ",getSysDescr(session))
-    print("---> System users number: ", getSysUsersNumber(session) )
-    print ("---> System date : ", getUptime(session))
-    totRam = getSysRam(session)
-    print("---> Total system Ram: ", totRam," Gb")   
-    print("---> Total system storage drive: ", getSysStorageDisk(session)," Gb")
-    print("---> Occupied system storage: ", getSysOccupiedDiskMemory(session)," Gb")
-    
-except:
-    print("Cannot retreive data, please check the connection and try again")
-    exit(1)
+print("Start COLLECTING DATA at: ", hostname," ",getSysDescr(session))
+print("---> System users number: ", getSysUsersNumber(session) )
+print ("---> System date : ", getUptime(session))
+totRam = getSysRam(session)
+print("---> Total system Ram: ", totRam," Gb")   
+print("---> Total system storage drive: ", getSysStorageDisk(session)," Gb")
+print("---> Occupied system storage: ", getSysOccupiedDiskMemory(session)," Gb")
    
 print("---> For checking processes, busy Ram and free Ram in real time open the .png files with associated names in reports/ ") 
 print("+++++++++++++++ To stop collecting press: Ctrl+c +++++++++++++++")
@@ -135,12 +132,10 @@ if (os.path.exists("reports/proc.rrd")) == False:
 if (os.path.exists("reports/ram.rrd")) == False:
     rrdtool.create("reports/ram.rrd", "--start", "now", "--step", "2", "RRA:AVERAGE:0.5:1:18", "DS:ram:GAUGE:10:1:"+ str(totRam))
    
-
 ## Virtual memory rrd data
 if (os.path.exists("reports/freeRam.rrd")) == False:
     rrdtool.create("reports/freeRam.rrd", "--start", "now", "--step", "2", "RRA:AVERAGE:0.5:1:18", "DS:freeRam:GAUGE:10:1:"+ str(totRam))
   
-
 while(1):
     
     ## Collecting data
@@ -155,15 +150,15 @@ while(1):
   
     ## Graph for processes
     rrdtool.update("reports/proc.rrd","N:"+ str(nproc))
-    rrdtool.graph("reports/process_graph.png","--start","now-30sec","--end","now","DEF:in=reports/proc.rrd:proc:AVERAGE","LINE:in#0000ff:Active process")
+    rrdtool.graph("reports/process_graph.png","--start","now-30sec","--end","now","DEF:in=reports/proc.rrd:proc:AVERAGE","LINE:in#0000ff:Active and loaded process")
     
     ## Graph for busy Ram
     rrdtool.update("reports/ram.rrd","N:"+ str(nram))
-    rrdtool.graph("reports/ram_graph.png","--start","now-30sec","--end","now","DEF:in=reports/ram.rrd:ram:AVERAGE","LINE:in#ff1203:Used Ram")
+    rrdtool.graph("reports/ram_graph.png","--start","now-30sec","--end","now","DEF:in=reports/ram.rrd:ram:AVERAGE","LINE:in#ff1203:Used Ram in Gb")
    
     ## Graph for free Ram
     rrdtool.update("reports/freeRam.rrd","N:"+ str(oram))
-    rrdtool.graph("reports/freeRam_graph.png","--start","now-30sec","--end","now","DEF:in=reports/freeRam.rrd:freeRam:AVERAGE","LINE:in#00ff00:Free Ram")
+    rrdtool.graph("reports/freeRam_graph.png","--start","now-30sec","--end","now","DEF:in=reports/freeRam.rrd:freeRam:AVERAGE","LINE:in#00ff00:Free Ram in Gb")
  
     ## For not flood of requestes the agent sleep some seconds
     time.sleep(2)

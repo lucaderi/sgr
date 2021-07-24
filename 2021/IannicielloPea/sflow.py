a99daa46eb9be393eacc3150eea41d5da72794dc
StefanoPea <1998stefano@gmail.com>
2021-07-24 16:13:38 +0200

    add license

--- 2021/IannicielloPea/sflow.py
+++ 2021/IannicielloPea/sflow.py
@@ -1,3 +1,21 @@
+#   Copyright 2016-2016 Practical Code, LLC
+#   
+#      Licensed under the Apache License, Version 2.0 (the "License");
+#      you may not use this file except in compliance with the License.
+#      You may obtain a copy of the License at
+#   
+#          http://www.apache.org/licenses/LICENSE-2.0
+#   
+#      Unless required by applicable law or agreed to in writing, software
+#      distributed under the License is distributed on an "AS IS" BASIS,
+#      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#      See the License for the specific language governing permissions and
+#      limitations under the License.
+
+#   Owner:   https://github.com/auspex-labs/
+#   Website: https://www.auspex-labs.com/
+#   Email:   contact@auspex-labs.com
+
 from socket import AF_INET, AF_INET6, inet_ntop
 from struct import unpack
 from uuid import UUID

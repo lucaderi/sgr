e465bb416f2a7808c2a450808a7936eada8f8062
Simone <passe98@gmail.com>
2022-06-03 21:16:54 +0200

    Add confidence interval

--- 2022/SimonePassera/double_exponential_smoothing.py
+++ 2022/SimonePassera/double_exponential_smoothing.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3.7
 
 import sys
+import statistics as stat
+import math
 
 alpha = 0.9
 beta = 0.9
+z = 3.291
 
 def double_exponential_smoothing(series, alpha, beta):
     result = [series[0]]
@@ -27,4 +30,12 @@ series = []
 for i in range(1, len(sys.argv)):
     series.append(round(float(sys.argv[i]) * 10.0, 1))
 
-print("a " + str(double_exponential_smoothing(series, alpha, beta)) + " c")
+prediction = double_exponential_smoothing(series, alpha, beta)
+
+confidence = z * (stat.stdev(series) / math.sqrt(len(series)))
+
+upper_bound = round(prediction + confidence, 1)
+
+lower_bound = round(prediction - confidence, 1)
+
+print(str(lower_bound) + " " + str(prediction) + " " + str(upper_bound))
--- 2022/SimonePassera/tempManager.sh
+++ 2022/SimonePassera/tempManager.sh
@@ -6,7 +6,10 @@ if [ ! -f "$FILE" ]; then
 	# Create db
 	rrdtool create $FILE \
 	--step 300 \
-	DS:tmp:GAUGE:600:15:50 \
+	DS:tmp:GAUGE:360:10:60 \
+	DS:low:GAUGE:360:10:60 \
+	DS:pre:GAUGE:360:10:60 \
+	DS:upp:GAUGE:360:10:60 \
 	RRA:AVERAGE:0.5:1:144 \
 	RRA:AVERAGE:0.5:12:24 \
 	RRA:AVERAGE:0.5:288:7
@@ -17,13 +20,16 @@ HOSTNAME=localhost
 TMP_FILE=/tmp/12_values
 DES_PATH=double_exponential_smoothing.py
 
+
+first=1
+lower_bound=0
+prediction=0
+upper_bound=0
+
 while true; do
 	temp_value=$(snmpget -v1 -c $COMMUNITY $HOSTNAME nsExtendOutputFull.\"temp\" | cut -f 4 -d " ")
-	#rrdtool update $FILE N:$temp_value
 
-	#echo $temp_value
-
-	rrdtool fetch temp_db.rrd AVERAGE -r 300 -s -1h | tail -n +3 | head -n 12 | cut -f 2 -d " " > $TMP_FILE
+	rrdtool fetch temp_db.rrd AVERAGE -r 300 -s -2h | tail -n +3 | cut -f 2 -d " " > $TMP_FILE
 
 	input_values=""
 
@@ -34,12 +40,39 @@ while true; do
 		fi
 	done < $TMP_FILE
 
-	# (lower_bound, prediction, upper_bound)
-	output_values=( $(./$DES_PATH $input_values) )
+	if [ $(echo $input_values | wc -w) -ge 12 ]; then
+		# (lower_bound, prediction, upper_bound)
+		output_values=( $(./$DES_PATH $input_values) )
 
-	echo ${output_values[1]}
+		if [ $first -eq 1 ]; then
+			first=0
+			
+			lower_bound=${output_values[0]}
+			prediction=${output_values[1]}
+			upper_bound=${output_values[2]}
+			
+			rrdupdate $FILE -t tmp N:$temp_value
+		else
+			rrdupdate $FILE -t tmp:low:pre:upp N:$temp_value:$lower_bound:$prediction:$upper_bound
+
+			lower_bound=${output_values[0]}
+			prediction=${output_values[1]}
+			upper_bound=${output_values[2]}
+		fi		
+	else
+		rrdupdate $FILE -t tmp N:$temp_value
+	fi
+	
+	rrdtool graph /var/www/html/temp_graph.png \
+	DEF:tmp=temp_db.rrd:tmp:AVERAGE \
+	DEF:pre=temp_db.rrd:pre:AVERAGE \
+	DEF:low=temp_db.rrd:low:AVERAGE \
+	DEF:upp=temp_db.rrd:upp:AVERAGE \
+	LINE1:tmp#1dd1a1:Temperature \
+	LINE1:pre#54a0ff:Prediction \
+	LINE1:upp#ee5253:Upper_bound \
+	LINE1:low#576574:Lower_bound \
+	-s -1h -e +5min > /dev/null
 	
 	sleep 300
 done
-
-#rrdtool graph temp_graph.png DEF:tmp=temp_db.rrd:tmp:AVERAGE LINE1:tmp#00ff00:Temperature --start -1h

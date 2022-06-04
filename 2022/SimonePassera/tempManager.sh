40fc40efe0693ce610e30bf9944fb454a40244e7
Simone <passe98@gmail.com>
2022-06-04 18:33:40 +0200

    Update prediction and Add telegram alert

--- 2022/SimonePassera/tempManager.sh
+++ 2022/SimonePassera/tempManager.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
+# rrd
 FILE=/home/pi/temp/temp_db.rrd
+GRAPH=/var/www/html/temp_graph.png
+# snmp
+COMMUNITY=public
+HOSTNAME=localhost
+# telegram
+TOKEN=XXXXX
+CHAT_ID=-1001733889922
+MAX_LIMIT=30
+# temporary file with last temperature values
+TMP_FILE=/tmp/last_values
+# path to code python
+DES_PATH=double_exponential_smoothing.py
 
 if [ ! -f "$FILE" ]; then
 	# Create db
@@ -15,21 +28,10 @@ if [ ! -f "$FILE" ]; then
 	RRA:AVERAGE:0.5:288:7
 fi
 
-COMMUNITY=public
-HOSTNAME=localhost
-TMP_FILE=/tmp/12_values
-DES_PATH=double_exponential_smoothing.py
-
-
-first=1
-lower_bound=0
-prediction=0
-upper_bound=0
-
 while true; do
 	temp_value=$(snmpget -v1 -c $COMMUNITY $HOSTNAME nsExtendOutputFull.\"temp\" | cut -f 4 -d " ")
 
-	rrdtool fetch temp_db.rrd AVERAGE -r 300 -s -2h | tail -n +3 | cut -f 2 -d " " > $TMP_FILE
+	rrdtool fetch $FILE AVERAGE -r 300 -s -2h | tail -n +3 | cut -f 2 -d " " > $TMP_FILE
 
 	input_values=""
 
@@ -40,30 +42,28 @@ while true; do
 		fi
 	done < $TMP_FILE
 
-	if [ $(echo $input_values | wc -w) -ge 12 ]; then
+	if [ $(echo $input_values | wc -w) -ge 2 ]; then
 		# (lower_bound, prediction, upper_bound)
 		output_values=( $(./$DES_PATH $input_values) )
 
-		if [ $first -eq 1 ]; then
-			first=0
-			
-			lower_bound=${output_values[0]}
-			prediction=${output_values[1]}
-			upper_bound=${output_values[2]}
-			
-			rrdupdate $FILE -t tmp N:$temp_value
-		else
-			rrdupdate $FILE -t tmp:low:pre:upp N:$temp_value:$lower_bound:$prediction:$upper_bound
+		rrdupdate $FILE -t tmp:low:pre:upp N:$temp_value:${output_values[0]}:${output_values[1]}:${output_values[2]}
 
-			lower_bound=${output_values[0]}
-			prediction=${output_values[1]}
-			upper_bound=${output_values[2]}
-		fi		
+		if [ $(echo "$temp_value > ${output_values[2]}" | bc -l) -eq 1 ]; then
+			curl -s -X POST https://api.telegram.org/bot$TOKEN/sendMessage -d chat_id=$CHAT_ID -d parse_mode=html -d text="<strong>Alert</strong> - Temperatura: <b>$temp_value</b>°C > ${output_values[2]}°C" > /dev/null
+		else
+			if [ $(echo "$temp_value < ${output_values[0]}" | bc -l) -eq 1 ]; then 
+				curl -s -X POST https://api.telegram.org/bot$TOKEN/sendMessage -d chat_id=$CHAT_ID -d parse_mode=html -d text="<strong>Alert</strong> - Temperatura: <b>$temp_value</b>°C < ${output_values[0]}°C" > /dev/null
+			fi
+		fi	
 	else
 		rrdupdate $FILE -t tmp N:$temp_value
 	fi
+
+	if [ $(echo "$temp_value > $MAX_LIMIT" | bc -l) -eq 1 ]; then
+		curl -s -X POST https://api.telegram.org/bot$TOKEN/sendMessage -d chat_id=$CHAT_ID -d parse_mode=html -d text="<strong>DANGER</strong> : Temperatura: <b>$temp_value</b>°C > LIMIT = <b>$MAX_LIMIT</b>°C" > /dev/null
+	fi
 	
-	rrdtool graph /var/www/html/temp_graph.png \
+	rrdtool graph $GRAPH \
 	DEF:tmp=temp_db.rrd:tmp:AVERAGE \
 	DEF:pre=temp_db.rrd:pre:AVERAGE \
 	DEF:low=temp_db.rrd:low:AVERAGE \
@@ -72,7 +72,8 @@ while true; do
 	LINE1:pre#54a0ff:Prediction \
 	LINE1:upp#ee5253:Upper_bound \
 	LINE1:low#576574:Lower_bound \
-	-s -1h -e +5min > /dev/null
+	HRULE:$MAX_LIMIT#a29bfe:Max_limit \
+	-s -1h -e +5min --vertical-label "temperature (°C)" > /dev/null
 	
 	sleep 300
 done

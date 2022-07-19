9b7a17da390a22c05bd9d11cb34878a643263963
funesca <funa.france@gmail.com>
2022-07-19 09:50:37 +0200

    more randomised data

--- 2022/Funaioli/create_rrd.sh
+++ 2022/Funaioli/create_rrd.sh
@@ -16,40 +16,84 @@ rrdtool create $db --step=1 --start=now-1800s\
 
 high=1
 value=0
-
 now=$(expr $(date "+%s") - 1800)
 
-for i in {0..1800}; do
+if [[ $1 -eq 0 ]]; then
 
-	if [ $i -ge 1500 ]; then
-		increment=$((1 + RANDOM % 10 + 20))
-	elif [ $i -ge 1200 ]; then
-		if [ $high -eq 1 ]; then
-			increment=$((1 + RANDOM % 5 + 40))
-			high=0
-		else
-			increment=$((1 + RANDOM % 5 + 5))
-			high=1
+	for i in {0..1800}; do
+		high=$((RANDOM % 2))
+		if [ $i -ge 1500 ]; then
+			increment=$((1 + RANDOM % 10 + 20))
+		elif [ $i -ge 1200 ]; then
+			if [ $high -eq 1 ]; then
+				increment=$((1 + RANDOM % 5 + 40))
+				#high=0
+			else
+				increment=$((1 + RANDOM % 5 + 5))
+				#high=1
+			fi
+		elif [ $i -ge 900 ]; then
+			increment=$((1 + RANDOM % 10 + 20))
+		elif [ $i -ge 600 ]; then
+			if [ $high -eq 1 ]; then
+				increment=$((1 + RANDOM % 5 + 40))
+				#high=0
+			else
+				increment=$((1 + RANDOM % 5 + 5))
+				#high=1
+			fi
+		else increment=$((1 + RANDOM % 10 + 20))
 		fi
-	elif [ $i -ge 900 ]; then
-		increment=$((1 + RANDOM % 10 + 20))
-	elif [ $i -ge 600 ]; then
-		if [ $high -eq 1 ]; then
-			increment=$((1 + RANDOM % 5 + 40))
-			high=0
-		else
-			increment=$((1 + RANDOM % 5 + 5))
-			high=1
+
+		(( value+=increment ))
+
+		rrdtool update $db ${now}:$value
+		now=$(expr ${now} + 1)
+	done
+
+elif [[ $1 -eq 1 ]]; then
+
+	for i in {0..1800}; do
+		high=$((RANDOM % 2))
+		if [ $i -ge 1500 ]; then
+			increment=$((1 + RANDOM % 5 + 20))
+		elif [ $i -ge 1320 ]; then
+			if [ $high -eq 1 ]; then
+				increment=$((1 + RANDOM % 3 + 30))
+				#high=0
+			else
+				increment=$((1 + RANDOM % 3 + 12))
+				#high=1
+			fi
+		elif [ $i -ge 1020 ]; then
+			increment=$((1 + RANDOM % 5 + 20))
+		elif [ $i -ge 840 ]; then
+			if [ $high -eq 1 ]; then
+				increment=$((1 + RANDOM % 3 + 40))
+				#high=0
+			else
+				increment=$((1 + RANDOM % 3 + 25))
+				#high=1
+			fi
+		elif [ $i -ge 540 ]; then
+			increment=$((1 + RANDOM % 5 + 20))
+		elif [ $i -ge 360 ]; then
+			if [ $high -eq 1 ]; then
+				increment=$((1 + RANDOM % 3 + 17))
+				#high=0
+			else
+				increment=$((1 + RANDOM % 3 + 2))
+				#high=1
+			fi
+		else increment=$((1 + RANDOM % 5 + 20))
 		fi
-	else increment=$((1 + RANDOM % 10 + 20))
-	fi
 
-	(( value+=increment ))
+		(( value+=increment ))
 
-	rrdtool update $db ${now}:$value
-	now=$(expr ${now} + 1)
-done
-
+		rrdtool update $db ${now}:$value
+		now=$(expr ${now} + 1)
+	done
+fi
 
 graph=1
 img=skeweddata.png
@@ -57,30 +101,12 @@ img=skeweddata.png
 if [ $graph -eq 1 ]; then
 
 	rrdtool graph $img -s -2000s \
-	-t "Network traffic" -z \
+	-t "Skewed data $1" -z \
 	-c "BACK#FFFFFF" -c "SHADEA#FFFFFF" -c "SHADEB#FFFFFF" \
 	-c "MGRID#AAAAAA" -c "GRID#CCCCCC" -c "ARROW#333333" \
 	-c "FONT#333333" -c "AXIS#333333" -c "FRAME#333333" \
-	-h 200 -w 1800 -l 0 -a PNG -v "B/s" \
+	-h 200 -w 2000 -l 0 -a PNG \
 	DEF:in=$db:in:AVERAGE \
-	VDEF:minin=in,MINIMUM \
-	VDEF:maxin=in,MAXIMUM \
-	VDEF:avgin=in,AVERAGE \
-	VDEF:lstin=in,LAST \
-	VDEF:totin=in,TOTAL \
-	"COMMENT: \l" \
-	"COMMENT:               " \
-	"COMMENT:Minimum      " \
-	"COMMENT:Maximum      " \
-	"COMMENT:Average      " \
-	"COMMENT:Current      " \
-	"COMMENT:Total        \l" \
-	"COMMENT:   " \
-	"LINE1:in#49BEEF" \
-	"GPRINT:minin:%5.1lf %sB/s   " \
-	"GPRINT:maxin:%5.1lf %sB/s   " \
-	"GPRINT:avgin:%5.1lf %sB/s   " \
-	"GPRINT:lstin:%5.1lf %sB/s   " \
-	"GPRINT:totin:%5.1lf %sB   \l" > /dev/null
+	"LINE1:in#49BEEF" > /dev/null
 
 fi

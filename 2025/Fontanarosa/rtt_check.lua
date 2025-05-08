d3e5e5811a8b3c649b7c2f0d79235405789110fc
Nicolò Fontanarosa <Nickcompanyofficial@gmail.com>
2025-05-08 18:27:59 +0200

    Update rtt_check.lua
    Ho modificato il path del file txt. invece di usare il path relativo ho usato il path assoluto

--- 2025/Fontanarosa/rtt_check.lua
+++ 2025/Fontanarosa/rtt_check.lua
@@ -125,8 +125,9 @@ function create_rtt_reference(file_path)
 
 end
 
--- Default è il percorso di Wireshark
-local file_path = "ntp_rtt_stats.txt"
+-- Determina il separatore di directory in base al sistema operativo
+local separator = package.config:sub(1,1)  -- '/' su Unix-like, '\' su Windows
+local file_path = Dir.global_config_path() .. separator .. "ntp_rtt_stats.txt"
 local rtt_reference = create_rtt_reference(file_path)
 
 --for country_code, data in pairs(rtt_reference) do

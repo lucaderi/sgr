99c1c917beffc6f046afe599f92a15b0f3f2ea95
Damiano <119160025+DamDev00@users.noreply.github.com>
2026-07-05 10:49:40 +0200

    Update pcount.c

--- 2026/Ottaviano/pcount.c
+++ 2026/Ottaviano/pcount.c
@@ -67,40 +67,6 @@ int32_t gmt_to_local(time_t t) {
   return (dt);
 }
 
-char* format_numbers(double val, char *buf, unsigned int buf_len, u_int8_t add_decimals)	{
-  unsigned int a1 = ((unsigned long)val / 1000000000) % 1000;	
-  unsigned int a = ((unsigned long)val / 1000000) % 1000;
-  unsigned int b = ((unsigned long)val / 1000) % 1000;
-  unsigned int c = (unsigned long)val % 1000;
-  unsigned int d = (unsigned int)((val - (unsigned long)val)*100) % 100;
-
-  if(add_decimals) {
-    if(val >= 1000000000) {
-      snprintf(buf, buf_len, "%u'%03u'%03u'%03u.%02d", a1, a, b, c, d);
-    } else if(val >= 1000000) {
-      snprintf(buf, buf_len, "%u'%03u'%03u.%02d", a, b, c, d);
-    } else if(val >= 100000) {
-      snprintf(buf, buf_len, "%u'%03u.%02d", b, c, d);
-    } else if(val >= 1000) {
-      snprintf(buf, buf_len, "%u'%03u.%02d", b, c, d);
-    } else
-      snprintf(buf, buf_len, "%.2f", val);
-  } else {
-    if(val >= 1000000000) {
-      snprintf(buf, buf_len, "%u'%03u'%03u'%03u", a1, a, b, c);
-    } else if(val >= 1000000) {
-      snprintf(buf, buf_len, "%u'%03u'%03u", a, b, c);
-    } else if(val >= 100000) {
-      snprintf(buf, buf_len, "%u'%03u", b, c);
-    } else if(val >= 1000) {
-      snprintf(buf, buf_len, "%u'%03u", b, c);
-    } else
-      snprintf(buf, buf_len, "%u", (unsigned int)val);
-  }
-
-  return(buf);
-}
-
 /* *************************************** */
 
 int drop_privileges(const char *username) {
@@ -133,197 +99,6 @@ int drop_privileges(const char *username) {
   umask(0);
   return 0;
 }
-
-/* *************************************** */
-/*
- * The time difference in microseconds
- */
-long delta_time (struct timeval * now,
-                 struct timeval * before) {
-  time_t delta_seconds;
-  time_t delta_microseconds;
-
-  /*
-   * compute delta in second, 1/10's and 1/1000's second units
-   */
-  delta_seconds      = now -> tv_sec  - before -> tv_sec;
-  delta_microseconds = now -> tv_usec - before -> tv_usec;
-
-  if(delta_microseconds < 0) {
-    /* manually carry a one from the seconds field */
-    delta_microseconds += 1000000;  /* 1e6 */
-    -- delta_seconds;
-  }
-  return((delta_seconds * 1000000) + delta_microseconds);
-}
-
-/* ******************************** */
-
-void print_stats() {
-  struct pcap_stat pcapStat;
-  struct timeval endTime;
-  float deltaSec;
-  static u_int64_t lastPkts = 0;
-  u_int64_t diff;
-  static struct timeval lastTime;
-  char buf1[64], buf2[64];
-
-  if(startTime.tv_sec == 0) {
-    lastTime.tv_sec = 0;
-    gettimeofday(&startTime, NULL);
-    return;
-  }
-
-  gettimeofday(&endTime, NULL);
-  deltaSec = (double)delta_time(&endTime, &startTime)/1000000;
-
-  if(pcap_stats(pd, &pcapStat) >= 0) {
-    fprintf(stderr, "=========================\n"
-	    "Absolute Stats: [%u pkts rcvd][%u pkts dropped]\n"
-	    "Total Pkts=%u/Dropped=%.1f %%\n",
-	    pcapStat.ps_recv, pcapStat.ps_drop, pcapStat.ps_recv-pcapStat.ps_drop,
-	    pcapStat.ps_recv == 0 ? 0 : (double)(pcapStat.ps_drop*100)/(double)pcapStat.ps_recv);
-    fprintf(stderr, "%llu pkts [%.1f pkt/sec] - %llu bytes [%.2f Mbit/sec]\n",
-	    numPkts, (double)numPkts/deltaSec,
-	    numBytes, (double)8*numBytes/(double)(deltaSec*1000000));
-
-    if(lastTime.tv_sec > 0) {
-      deltaSec = (double)delta_time(&endTime, &lastTime)/1000000;
-      diff = numPkts-lastPkts;
-      fprintf(stderr, "=========================\n"
-	      "Actual Stats: %s pkts [%.1f ms][%s pkt/sec]\n",
-	      format_numbers(diff, buf1, sizeof(buf1), 0), deltaSec*1000,
-	      format_numbers(((double)diff/(double)(deltaSec)), buf2, sizeof(buf2), 1));
-      lastPkts = numPkts;
-    }
-
-    fprintf(stderr, "=========================\n");
-  }
-
-  lastTime.tv_sec = endTime.tv_sec, lastTime.tv_usec = endTime.tv_usec;
-}
-
-/* ******************************** */
-
-void sigproc(int sig) {
-  static int called = 0;
-
-  fprintf(stderr, "Leaving...\n");
-  if (called) return; else called = 1;
-
-  pcap_breakloop(pd);
-}
-
-/* ******************************** */
-
-void my_sigalarm(int sig) {
-  print_stats();
-  alarm(ALARM_SLEEP);
-  signal(SIGALRM, my_sigalarm);
-}
-
-/* ****************************************************** */
-
-static char hex[] = "0123456789ABCDEF";
-
-char* etheraddr_string(const unsigned char *ep, char *buf) {
-  unsigned int i, j;
-  char *cp;
-
-  cp = buf;
-  if ((j = *ep >> 4) != 0)
-    *cp++ = hex[j];
-  else
-    *cp++ = '0';
-
-  *cp++ = hex[*ep++ & 0xf];
-
-  for(i = 5; (int)--i >= 0;) {
-    *cp++ = ':';
-    if ((j = *ep >> 4) != 0)
-      *cp++ = hex[j];
-    else
-      *cp++ = '0';
-
-    *cp++ = hex[*ep++ & 0xf];
-  }
-
-  *cp = '\0';
-  return (buf);
-}
-
-/* ****************************************************** */
-
-/*
- * A faster replacement for inet_ntoa().
- */
-char* __intoa(unsigned int addr, char* buf, unsigned short bufLen) {
-  char *cp, *retStr;
-  unsigned int byte;
-  int n;
-
-  cp = &buf[bufLen];
-  *--cp = '\0';
-
-  n = 4;
-  do {
-    byte = addr & 0xff;
-    *--cp = byte % 10 + '0';
-    byte /= 10;
-    if (byte > 0) {
-      *--cp = byte % 10 + '0';
-      byte /= 10;
-      if (byte > 0)
-	*--cp = byte + '0';
-    }
-    *--cp = '.';
-    addr >>= 8;
-  } while (--n > 0);
-
-  /* Convert the string to lowercase */
-  retStr = (char*)(cp+1);
-
-  return(retStr);
-}
-
-/* ************************************ */
-
-static char buf[sizeof "ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff"];
-
-char* intoa(unsigned int addr) {
-  return(__intoa(addr, buf, sizeof(buf)));
-}
-
-/* ************************************ */
-
-static inline char* in6toa(struct in6_addr addr6) {
-  snprintf(buf, sizeof(buf),
-	   "%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x",
-	   addr6.s6_addr[0], addr6.s6_addr[1], addr6.s6_addr[2],
-	   addr6.s6_addr[3], addr6.s6_addr[4], addr6.s6_addr[5], addr6.s6_addr[6],
-	   addr6.s6_addr[7], addr6.s6_addr[8], addr6.s6_addr[9], addr6.s6_addr[10],
-	   addr6.s6_addr[11], addr6.s6_addr[12], addr6.s6_addr[13], addr6.s6_addr[14],
-	   addr6.s6_addr[15]);
-
-  return(buf);
-}
-
-/* ****************************************************** */
-
-char* proto2str(unsigned short proto) {
-  static char protoName[8];
-
-  switch(proto) {
-  case IPPROTO_TCP:  return("TCP");
-  case IPPROTO_UDP:  return("UDP");
-  case IPPROTO_ICMP: return("ICMP");
-  default:
-    snprintf(protoName, sizeof(protoName), "%d", proto);
-    return(protoName);
-  }
-}
-
-/* ****************************************************** */
 
 static int32_t thiszone;
 int count = 0;
@@ -367,63 +142,27 @@ void dummyProcesssPacket(unsigned char *_deviceId,
             printf("pacchetto inoltrato\n");
           }
           printf("===================\n\n");
-  }
-    if(dumper)
-      pcap_dump((unsigned char*)dumper, (struct pcap_pkthdr*)h, p);
+  } else {
+
+    /*
+      ricezione di un pacchetto del normale traffico di rete
+    */
+    count++;
+    printf("===================\ncounter: %d\n",count);
+    printf("pacchetto arrivato!\n");
   
-  if(verbose) {
-    struct ether_header ehdr;
-    unsigned short eth_type, vlan_id;
-    char buf1[32], buf2[32];
-    struct ip ip;
-    struct ip6_hdr ip6;
-
-    int s = (h->ts.tv_sec + thiszone) % 86400;
-
-    printf("%02d:%02d:%02d.%06u ",
-	   s / 3600, (s % 3600) / 60, s % 60,
-	   (unsigned)h->ts.tv_usec);
-
-    memcpy(&ehdr, p, sizeof(struct ether_header));
-    eth_type = ntohs(ehdr.ether_type);
-    printf("[%s -> %s] ",
-	   etheraddr_string(ehdr.ether_shost, buf1),
-	   etheraddr_string(ehdr.ether_dhost, buf2));
-
-    if(eth_type == 0x8100) {
-      vlan_id = (p[14] & 15)*256 + p[15];
-      eth_type = (p[16])*256 + p[17];
-      printf("[vlan %u] ", vlan_id);
-      p+=4;
-    }
-    if(eth_type == 0x0800) {
-      memcpy(&ip, p+sizeof(ehdr), sizeof(struct ip));
-      printf("[%s]", proto2str(ip.ip_p));
-      printf("[%s ", intoa(ntohl(ip.ip_src.s_addr)));
-      printf("-> %s] ", intoa(ntohl(ip.ip_dst.s_addr)));
-    } else if(eth_type == 0x86DD) {
-      memcpy(&ip6, p+sizeof(ehdr), sizeof(struct ip6_hdr));
-      printf("[%s ", in6toa(ip6.ip6_src));
-      printf("-> %s] ", in6toa(ip6.ip6_dst));
-    } else if(eth_type == 0x0806)
-      printf("[ARP]");
-    else
-      printf("[eth_type=0x%04X]", eth_type);
-
-    printf("[caplen=%u][len=%u]\n", h->caplen, h->len);
+    int send = pcap_sendpacket(pd, p, 
+          h->caplen);
+          if(send != 0){
+            fprintf(stderr, "Errore invio pacchetto: %s\n", pcap_geterr(pd));
+          } else {
+            printf("pacchetto inoltrato\n");
+          }
+          printf("===================\n\n");
   }
-
-  if(numPkts == 0) gettimeofday(&startTime, NULL);
-  numPkts++, numBytes += h->len;
-
-  if(verbose == 2) {
-      int i;
-
-      for(i = 0; i < h->caplen; i++)
-        printf("%02X ", p[i]);
-      printf("\n");
-  }
- }
+   
+  
+}
 
 /* *************************************** */
 
@@ -434,6 +173,7 @@ void printHelp(void) {
   printf("Usage: pcount [-h] -i <device|path> [-w <path>] [-f <filter>] [-l <len>] [-v <1|2>]\n");
   printf("-h               [Print help]\n");
   printf("-i <device|path> [Device name or file path]\n");
+  printf("-d <flagIdPacket> [flag identification packet]\n");
   printf("-f <filter>      [pcap filter]\n");
   printf("-w <path>        [pcap write file]\n");  
   printf("-d <num>        [packet id]\n");  
@@ -558,7 +298,7 @@ int main(int argc, char* argv[]) {
 
   pcap_loop(pd, -1, dummyProcesssPacket, NULL);
 
-  print_stats();
+  
 
   pcap_close(pd);
 
